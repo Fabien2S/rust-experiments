dc29705cd9ca6aa463d056d9989c011b1c10732f
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2022-03-08 22:59:41 +0100

    Add borrowing example

--- hello_functions/src/main.rs
+++ hello_functions/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str = String::new();
+    str.push_str("Hello");
+    str.push_str(", world!");
+
+    print_my_string(&str);
+
+    str.push_str(" It's working!");
+    print_my_string(&str);
+
     if is_result_of_x_by_y_larger_than_x(2, 4) {
         println!("Larger than x!")
     } else {
         println!("Smaller than or equal to x!")
     }
+}
+
+fn print_my_string(str: &String) {
+    println!("{}", str);
 }
 
 fn is_result_of_x_by_y_larger_than_x(x: i32, y: i32) -> bool {
